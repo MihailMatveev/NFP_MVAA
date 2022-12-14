a6ebb1766cb79c257de60b7bf62f969dd1a4285d
Михаил Матвеев <107339955+MihailMatveev@users.noreply.github.com>
2022-12-14 13:06:59 +0300

    необходимо с БД сделать вход

---
+++ ViewModes/Entry/RegisterUserViewModel.cs
@@ -0,0 +1,30 @@
+﻿using NFP_MVAA.Infastructure;
+using NFP_MVAA.Views.Windows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace NFP_MVAA.ViewModes.Entry
+{
+    public class RegisterUserViewModel : ViewModes.Base.ViewModelBase
+    {
+        #region Commands
+        public ICommand CloseApplicationCommand { get; set; }
+
+        private void OnCloseApplicationCommandExecute(object p) => Application.Current.Shutdown(); 
+        
+
+
+        #endregion
+
+
+        public RegisterUserViewModel()
+        {
+            CloseApplicationCommand = new LamdaCommand(OnCloseApplicationCommandExecute);
+        }
+    }
+}
