094403f9ecb9d6453ac6144fd20d4c5a53231ed6
Михаил Матвеев <107339955+MihailMatveev@users.noreply.github.com>
2022-12-15 18:42:20 +0300

    Настроил Validation на имя при входе

---
+++ Models/Valid/MinCharacterRule.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace NFP_MVAA.Models.Valid
+{
+    public class MinCharacterRule : ValidationRule
+    {
+
+        public int MinCharacter { get; set; }
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            var charstring=value as string;
+            if (charstring.Length < MinCharacter)
+                return new ValidationResult(false, $"Минимальное количество символов {MinCharacter}");
+            return new ValidationResult(true, null);
+        }
+    }
+}
--- ViewModes/Entry/RegisterUserViewModel.cs
+++ ViewModes/Entry/RegisterUserViewModel.cs
@@ -4,6 +4,7 @@ using NFP_MVAA.Views.Windows;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,60 +13,26 @@ using System.Windows.Input;
 
 namespace NFP_MVAA.ViewModes.Entry
 {
-    public class RegisterUserViewModel : ViewModes.Base.ViewModelBase,IDataErrorInfo
+    public class RegisterUserViewModel : ViewModes.Base.ViewModelBase
     {
 
         private string userName;
+
+
         public string UserName
         {
             get { return userName; }
-            set
-            {
-               
-                Set(ref userName, value);
-            }
+            set=> Set(ref userName, value);
         }
+
+
 
 
         #region Commands
         public ICommand CloseApplicationCommand { get; set; }
 
 
-        #region Validation
-        public string Error { get { return null; } }
-
-        private string errorMessege;
-        public string ErrorMessege
-        {
-            get { return errorMessege; }
-            set { Set(ref errorMessege, value); }
-        }
-
-        public string this[string columnName]
-        {
-            get
-            {
-                string result = null;
-                switch (columnName)
-                {
-                    case "UserName":
-                        if (string.IsNullOrEmpty(UserName))
-                            result = "Введите имя";
-                        else if (UserName.Length<=5)  //проверка на наличие пользователя
-                            result = "Количество символов меньше 6";
-                        break;
-
-                    default:
-                        break;
-                }
-
-                ErrorMessege=result;
-
-                return result;
-            }
-        }
-
-        #endregion
+      
 
         private void OnCloseApplicationCommandExecute(object p) => Application.Current.Shutdown(); 
 
